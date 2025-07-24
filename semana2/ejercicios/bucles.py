a58beb1d8423b1ef06fa92823e07ad42dbc858db
Bruno Henríquez Cano <bruno.henriquez.1993@gmail.com>
2025-07-24 14:27:04 +0000

    Basico

--- semana2/ejercicios/bucles.py
+++ semana2/ejercicios/bucles.py
@@ -2,9 +2,18 @@
 
 lista_de_nombres = ["ale", "luis", "alberto"]
 
-print(lista_de_nombres[0])
-print(lista_de_nombres[1])
-print(lista_de_nombres[2])
+#numeros = [1,2,3,4,5,6,7,8,9,10]
+limite = int(input("hasta que numero quieres imprimiren el buzz: "))
+numeros = range(1, limite +1) #imprime de la segunda pocicion que seria el numero 1 [0][1] al 10
 
-for nombre in lista_de_nombres:
-    print(nombre)
+
+
+for numero in numeros:
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("fizz buzz")
+    elif numero % 3 == 0:  # Devuelve implicitamente true o false, si es false, reliza la siguiente comparacion, si es true, imprime el resultado
+        print("es fizz")   
+    elif numero % 5 == 0: # realiza el mismo paso anterior, pero no continua en la secuencia del if, sino que simplemente, al ser todas las respuestas false, actua else.
+        print("buzz")
+    else:
+        print(numero) 
