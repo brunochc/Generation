9b40e8cbd1171ef7b0b9b39ff24356311d2c879e
Bruno Henríquez Cano <bruno.henriquez.1993@gmail.com>
2025-08-06 15:30:29 +0000

    semana 4

---
+++ semana4/index.js
@@ -0,0 +1,18 @@
+/*
+todo lo que este dentro de estos caracteres se condciderara comentarios
+
+bien
+
+
+*/
+
+
+let mivariable  // declarar variable --> js asigna un tipo de dato undefinied
+mivariable = "hola mundo" // reasignacion de valor (en este caso string)
+
+console.log(mivariable)
+
+
+// prom es el equivalente de input en python
+
+let name = prompt("ingrese su nombre: ")
